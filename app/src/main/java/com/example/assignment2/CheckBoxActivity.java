fa25543662a3ca3bffd73a3b0e18e732bf031ef4
adnanshad35 <adnanshad1035@gmail.com>
2024-12-01 09:54:19 +0600

    Final Commit Assignment 2

--- app/src/main/java/com/example/assignment2/CheckBoxActivity.java
+++ app/src/main/java/com/example/assignment2/CheckBoxActivity.java
@@ -1,12 +1,46 @@
 package com.example.assignment2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CheckBoxActivity extends AppCompatActivity {
+
+    CheckBox checkBox1, checkBox2, checkBox3;
+    Button showSelectedButton;
+    TextView selectedBooksText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkbox);
+
+        checkBox1 = findViewById(R.id.checkbox1);
+        checkBox2 = findViewById(R.id.checkbox2);
+        checkBox3 = findViewById(R.id.checkbox3);
+        showSelectedButton = findViewById(R.id.showSelectedButton);
+        selectedBooksText = findViewById(R.id.selectedBooksText);
+
+        showSelectedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringBuilder selectedBooks = new StringBuilder("Selected Books: ");
+
+                if (checkBox1.isChecked()) {
+                    selectedBooks.append("\n").append(checkBox1.getText().toString());
+                }
+                if (checkBox2.isChecked()) {
+                    selectedBooks.append("\n").append(checkBox2.getText().toString());
+                }
+                if (checkBox3.isChecked()) {
+                    selectedBooks.append("\n").append(checkBox3.getText().toString());
+                }
+
+                selectedBooksText.setText(selectedBooks.toString());
+            }
+        });
     }
 }
